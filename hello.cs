edbb195bd34d31c159c4ab3a65ce4911d6724a85
Mali Damjan <Damianopuglisi818@gmail.com>
2023-01-04 12:58:57 +0100

    razrešen konflikt

--- hello.cs
+++ hello.cs
@@ -3,10 +3,9 @@ public class Hello
    public static void Main()
    {
       System.Console.WriteLine("Hello, World!");
+      System.Console.WriteLine("Hello, github");
       System.Console.WriteLine("Hello, Local!");
-<<<<<<< HEAD
-      System.Console.WriteLine("Hello, Local!");
-=======
->>>>>>> 23ba380868c5e09f9ac7c03a94e1c8d62f5ee910
+
+
     }
 }
